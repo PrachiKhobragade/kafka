1ca4917db314a8ebf04846f8937d7790f0d8b02a
Prachi Khobragade <pkhobragade@linkedin.com>
2022-02-20 18:03:54 -0800

    fix merge conflicts

--- clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
+++ clients/src/main/java/org/apache/kafka/clients/producer/KafkaProducer.java
@@ -483,7 +483,7 @@ public class KafkaProducer<K, V> implements Producer<K, V> {
                 apiVersions,
                 throttleTimeSensor,
                 logContext,
-                producerConfig.getString(ProducerConfig.LI_CLIENT_SOFTWARE_NAME_AND_COMMIT_CONFIG)
+                producerConfig.getString(ProducerConfig.LI_CLIENT_SOFTWARE_NAME_AND_COMMIT_CONFIG),
                 leastLoadedNodeAlgorithm);
         short acks = configureAcks(producerConfig, log);
         return new Sender(logContext,
